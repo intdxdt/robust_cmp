909f3bddbc234b764007c19c254a636d82bc0a88
Titus Tienaah <titus.intdxdt@gmail.com>
2017-08-13 08:10:13 -0300

    cmp

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# robust_cmp
-compare 
+# robust_compare
+compares a pair of non-overlapping increasing sequences.
+
+# credits 
+After js-version by  [Mikola Lysenko](https://github.com/mikolalysenko/robust-compare)
---
+++ src/lib.rs
@@ -0,0 +1,17 @@
+extern crate robust_subtract;
+//Robust Compare
+pub fn robust_compare(a: &[f64], b: &[f64]) -> f64 {
+    *(robust_subtract::robust_subtract(a, b).last().unwrap())
+}
+
+
+#[cfg(test)]
+mod test_robust_compare{
+    use super::robust_compare as cmp;
+    #[test]
+    fn test_robust_cmp() {
+		      assert!(cmp(&vec!(5.),    &vec!(1., 4.)) == 0.);
+			  assert!(cmp(&vec!(1e64), &vec!(-1e-100, 1e64)) > 0.);
+			  assert!(cmp(&vec!(1e64), &vec!(1e-100, 1e64)) < 0.);
+    }
+}
